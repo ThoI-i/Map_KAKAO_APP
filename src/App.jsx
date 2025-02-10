7a0ec41713174a0bf4a6537495766a85903d5a17
Theo <ojw1912@gmail.com>
2025-02-10 11:42:30 +0900

    250210_#1-2_API 키 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    if (!window.Kakao.isInitialized()) {
+      window.Kakao.init('e69b47d018fbeaf74a60b7a5d68e2606');  // 카카오 앱 키로 초기화
+      console.log('Kakao SDK Initialized');
+    }
+  }, []);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <div>카카오 API 연동 예제</div>;
 }
 
-export default App
+export default App;
