5f3b34a79c9aff89b332c0b238527eb72fd58d04
Theo <ojw1912@gmail.com>
2025-03-27 21:21:34 +0900

    250327 #3-2-4[Refac2] Front: host: "___"와 Back .allowedOrigins("__"); 불일치로 인한 CORS ERROR

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    host: "127.0.0.1", // 또는 host: true
+    host: "localhost", // 또는 host: true
     port: 5173,
     proxy: {
       "/auth": {
