4ade27909f54c0f0928e11034f41167471ce0084
Theo <ojw1912@gmail.com>
2025-03-28 14:39:13 +0900

    250328 #3-2-4[Refac3] resolve: { alias: { } } 누락 Error 발생

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // ✅ 여기가 핵심!
+    },
+  },
   server: {
-    host: "localhost", // 또는 host: true
+    host: "localhost",
     port: 5173,
     proxy: {
       "/auth": {
