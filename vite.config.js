c4d7727f6a234cce4b4e2215fcee720479d8ee24
Theo <ojw1912@gmail.com>
2025-03-31 17:17:03 +0900

    250331 #3-2-4-A[Refac] Refresh Token O, Access Token 재생성 X [secure: false로 변경]

--- vite.config.js
+++ vite.config.js
@@ -16,12 +16,14 @@ export default defineConfig({
       "/user": {
         target: "http://localhost:9000",
         changeOrigin: true,
+        secure: false, // 쿠키/세션 사용 시 필수
         rewrite: (path) => path.replace(/^\/user/, "/user"),
       },
       // ✅ /api 요청 백엔드로 프록시
       "/api": {
         target: "http://localhost:9000",
         changeOrigin: true,
+        secure: false, // 쿠키/세션 사용 시 필수
         rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
     },
